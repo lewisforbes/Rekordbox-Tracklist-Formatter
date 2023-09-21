26db6fcb01213d580d96512cb32ee136ede3a975
Lewis Forbes <60221117+lewisforbes@users.noreply.github.com>
2023-09-21 11:57:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ for l in f:
   if first_line:
     first_line = False
     continue
+
   l = l.split("	")
-  output += "{} - {}\n".format(l[2], l[3].replace("/", ", "))
+  if len(l)!=1:
+    output += "{} - {}\n".format(l[2], l[3].replace("/", ", "))
 
 output = output[:len(output)-1]
+
 f.close()
 f = open('formatted_tracklist.txt', 'w')
 f.write(output)
 f.close()
 print(output)
+
+input("press enter to exit")
